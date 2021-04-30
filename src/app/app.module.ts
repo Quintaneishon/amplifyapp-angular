29f7186dab46af438d1355a2a5af4348adf639dc
Quintaneishon <ajitzi.quintana@gmail.com>
2021-04-30 11:40:58 -0500

    add amplify auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* import AmplifyUIAngularModule  */
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    /* configure app with AmplifyUIAngularModule */
+    AmplifyUIAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
